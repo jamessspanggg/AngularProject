3f87dfc5ec8ff4e4440d02c321c7c4fffd534232
jamessspanggg <jamespangmunwai@gmail.com>
2020-07-09 19:57:25 +0800

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
